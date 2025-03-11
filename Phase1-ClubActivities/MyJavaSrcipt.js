8a0b574c134182c8fc8f9d064a5b6a7d62bd4007
MohammedAlaradi <mohdaradi2004@gmail.com>
2025-03-11 17:27:20 +0300

    Made the filter interactive, commented the position of all elements to prevent the overlap

--- Phase1-ClubActivities/MyJavaSrcipt.js
+++ Phase1-ClubActivities/MyJavaSrcipt.js
@@ -3,3 +3,13 @@ function closeMenu(){document.getElementById("TheSideBar").style.width = "0px";}
 
 /* Open the navigation sidebar */
 function openMenu(){document.getElementById("TheSideBar").style.width="250px";}
+
+function openFilter(){
+  document.getElementById("filterForm").style.width="250px"
+  document.getElementById("filterForm").style.height="200px"
+}
+
+function closeFilter(){
+  document.getElementById("filterForm").style.width="0px"
+  document.getElementById("filterForm").style.height="0px"
+}
