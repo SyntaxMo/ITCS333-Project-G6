fd5e23a665f92801bdd39b54b1b4cf1f04ecb0e0
MohammedAlaradi <mohdaradi2004@gmail.com>
2025-03-19 14:27:55 +0300

    Added the structure of dropdown cards

--- Phase1-ClubActivities/MyJavaSrcipt.js
+++ Phase1-ClubActivities/MyJavaSrcipt.js
@@ -1,8 +1,14 @@
 /* Close the navigation sidebar */
-function closeMenu(){document.getElementById("TheSideBar").style.width = "0px";}
+function closeMenu(){
+  document.getElementById("TheSideBar").style.width = "0px";
+  document.getElementById("TheSideBar").style.height = "0%";
+}
 
 /* Open the navigation sidebar */
-function openMenu(){document.getElementById("TheSideBar").style.width="250px";}
+function openMenu(){
+  document.getElementById("TheSideBar").style.width="250px";
+  document.getElementById("TheSideBar").style.height = "100%";
+}
 
 function openFilter(){
   document.getElementById("filterForm").style.width="250px"
