045bc848d35785b540e22a18d717dd4102b0e9a7
MohammedAlaradi <mohdaradi2004@gmail.com>
2025-03-23 15:39:08 +0300

    Changed the cards completely, added a pagination and set the color of button to green

--- Phase1-ClubActivities/MyJavaSrcipt.js
+++ Phase1-ClubActivities/MyJavaSrcipt.js
@@ -1,21 +1,21 @@
 /* Close the navigation sidebar */
 function closeMenu(){
-  document.getElementById("TheSideBar").style.width = "0px";
-  document.getElementById("TheSideBar").style.height = "0%";
+  document.getElementById("the-side-bar").style.width = "0px";
+  document.getElementById("the-side-bar").style.height = "0%";
 }
 
 /* Open the navigation sidebar */
 function openMenu(){
-  document.getElementById("TheSideBar").style.width="250px";
-  document.getElementById("TheSideBar").style.height = "100%";
+  document.getElementById("the-side-bar").style.width="250px";
+  document.getElementById("the-side-bar").style.height = "100%";
 }
 
 function openFilter(){
-  document.getElementById("filterForm").style.width="250px"
-  document.getElementById("filterForm").style.height="200px"
+  document.getElementById("filter-form").style.width="250px"
+  document.getElementById("filter-form").style.height="200px"
 }
 
 function closeFilter(){
-  document.getElementById("filterForm").style.width="0px"
-  document.getElementById("filterForm").style.height="0px"
+  document.getElementById("filter-form").style.width="0px"
+  document.getElementById("filter-form").style.height="0px"
 }
