85be867e86ca0b1ba69068f9f2f834e3a681b2ed
MohammedAlaradi <mohdaradi2004@gmail.com>
2025-03-27 13:02:52 +0300

    Improved appearance

--- Phase1-ClubActivities/MyJavaSrcipt.js
+++ Phase1-ClubActivities/MyJavaSrcipt.js
@@ -1,11 +1,11 @@
 /* Close the navigation sidebar */
-function closeMenu(){
+function closeNavBar(){
   document.getElementById("the-side-bar").style.width = "0px";
   document.getElementById("the-side-bar").style.height = "0%";
 }
 
 /* Open the navigation sidebar */
-function openMenu(){
+function openNavBar(){
   document.getElementById("the-side-bar").style.width="250px";
   document.getElementById("the-side-bar").style.height = "100%";
 }
@@ -18,4 +18,14 @@ function openFilter(){
 function closeFilter(){
   document.getElementById("filter-form").style.width="0px"
   document.getElementById("filter-form").style.height="0px"
+}
+
+function openScheduleActivity(){
+  document.getElementById("Schedule-activity").style.width="450px"
+  document.getElementById("Schedule-activity").style.height="350px"
+}
+
+function closeScheduleActivity(){
+  document.getElementById("Schedule-activity").style.width="0px"
+  document.getElementById("Schedule-activity").style.height="0px"
 }
