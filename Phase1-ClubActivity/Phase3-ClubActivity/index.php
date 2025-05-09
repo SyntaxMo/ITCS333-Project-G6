c98e74781f60728214b5ae818d8f71a5ca74f8a7
MohammedAlaradi <mohdaradi2004@gmail.com>
2025-05-09 12:26:04 +0300

    made the get request, but IDK how to test locally.

--- Phase1-ClubActivity/Phase3-ClubActivity/index.php
+++ Phase1-ClubActivity/Phase3-ClubActivity/index.php
@@ -1,26 +1,14 @@
 <?php
 
-$conn = mysqli_connect ("localhost", "root", "", "clubactivity");
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "clubactivity";
+
+$conn = mysqli_connect($host, $username, $password, $dbname);
 $response = array();
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
-}
-else {
-  $sql = "SELECT * FROM activity";
-  $result = mysqli_query($conn, $sql);  
-  $i=0;
-  if($result){
-    header("content-type: JSON");
-    while ($row = mysqli_fetch_assoc($result)){
-    $response[$i]['title'] = $row['title'];
-    $response[$i]['host'] = $row['host'];
-    $response[$i]['location'] = $row['location'];
-    $response[$i]['dateTime'] = $row['dateTime'];
-    $response[$i]['description'] = $row['description'];
-    $i++;
-    }
-    echo json_encode($response, JSON_PRETTY_PRINT);
-  }
 }
 
 ?>
