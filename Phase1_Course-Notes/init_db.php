8fa7f10c8ea9b7108b3de99c5869248d06c8ed40
JaberKhalid11 <jaberbaloch14@gmail.com>
2025-05-14 22:42:46 +0300

    fixing bugs

--- Phase1_Course-Notes/init_db.php
+++ Phase1_Course-Notes/init_db.php
@@ -10,7 +10,7 @@ try {
     $pdo = new PDO("mysql:host=$host", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
-    $pdo->exec("DROP DATABASE IF EXISTS unihubb");
+   
     $pdo->exec("CREATE DATABASE IF NOT EXISTS unihubb");
     $pdo->exec("USE unihubb");
     
