d7fca22493113447add8cdef9d0b4916cebde0dd
Ayoub M. Alomari <ayoub.stu.bh@gmail.com>
2025-05-15 16:17:43 +0300

    Update config.php

--- Campus News/PHP/config.php
+++ Campus News/PHP/config.php
@@ -1,12 +1,13 @@
 <?php
 $config = [
-    'host' => 'localhost',
-    'dbname' => 'file_storage',
-    'username' => 'root',
-    'password' => '',
+    'host' => getenv('db_host'),
+    'dbname' => getenv('db_name'),
+    'username' => getenv('db_user'),
+    'password' => getenv('db_pass'),
     'options' => [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES => false,
     ]
 ];
+?>
